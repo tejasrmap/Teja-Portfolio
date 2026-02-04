b0524b5e1c56499bdb24928dcdfb1281807e6756
tejasrmap <tejag.vijay@gmail.com>
2026-02-04 10:13:17 +0530

    Added New Project

--- constants.tsx
+++ constants.tsx
@@ -6,7 +6,7 @@ const DEFAULT_DATA: PortfolioData = {
   role: "Founder & Director of GT Innovix",
   avatar: "https://i.pinimg.com/736x/49/09/3c/49093cd4230d03b5f70d26483138c9e7.jpg",
   bio: "Big Data Analyst | Full Stack Developer | Founder MD Of GT Innovix LLP",
-  fullBio: "I am Teja Ganugula, a Computer Science Engineering student specializing in Big Data Analytics at SRM University–AP. I am the Founder and Managing Director of GT Innovix LLP, where I actively work on transforming ideas into scalable solutions. As a team lead in multiple hackathons, I have led projects from concept to execution and presented startup pitches to diverse audiences. I am deeply interested in coding, exploring emerging technologies, and building innovative products within the entrepreneurship ecosystem.",
+  fullBio: "I am Teja Ganugula, a Computer Science Engineering student specializing in Big Data Analytics at SRM University–AP. I am the Founder and Managing Director of GT Innovix LLP, where I actively work on transforming ideas into scalable solutions. As a team lead in multiple hackathons, I have led projects from concept to execution and presented startup pitches to diverse audiences. I am deeply interested in coding, exploring emerging technologies, and building innovative products within the entrepreneurship ecosystem. ",
   nameScale: 1,
   sectionHeaders: {
     projects: { title: "PROJECTS", tagline: "// 01" },
@@ -22,7 +22,7 @@ const DEFAULT_DATA: PortfolioData = {
   },
   projects: [
     {
-      id: "SRM-01",
+      id: "GT-01",
       title: "CROWD MONITOR",
       description: "Built an intelligent crowd monitoring system enabling real-time analysis for safety and resource optimization.",
       longDescription: "",
@@ -31,13 +31,22 @@ const DEFAULT_DATA: PortfolioData = {
       links: { github: "https://github.com/tejasrmap/CrowdDetection-VibeCraft", redirect: "https://crowdmonitoring-vibecraft.netlify.app/"}
     },
     {
-      id: "SRM-02",
+      id: "GT-02",
       title: "AGRIRENT SITE",
       description: "A FrontEnd Website Made Baed On HTML JS CSS for Farmers to rent Agricultura Equipment from Equipment Owners Directly without Middlemen.",
       longDescription: ".",
       image: "https://i.pinimg.com/736x/0d/53/be/0d53bea9c5f2ef597597e7dafc235d5a.jpg",
       tags: ["HTML", "JS", "CSS"],
         links: { github: "https://github.com/tejasrmap/AgriRent_Website", redirect: "https://agrirent.tiiny.site/"}
+    },
+    {
+      id: "GT-03",
+      title: "Object Detection Using Live Camera Feed",
+      description: "This is made completely based on Python YOLOv8 and used Streamlit for Testing Purposes.",
+      longDescription: ".",
+      image: "https://i.pinimg.com/736x/1c/31/ba/1c31baaa43a760f1ae005158a5bdd040.jpg",
+      tags: ["Python", "YoloV8", "Streamlit", "CV2"],
+        links: { github: "https://github.com/tejasrmap/ObjectDetection-LiveFeed", redirect: "#"}
     }
   ],
   skills: [
