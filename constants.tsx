6bfecf9c347a32acfc885f568748999836bf8bac
tejasrmap <tejag.vijay@gmail.com>
2026-02-14 23:08:54 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -62,7 +62,7 @@ const DEFAULT_DATA: PortfolioData = {
       title: "PeaceXplorer - Work-Life Balancer.",
       description: "Developed PeaceXplorer, a human-centered wellbeing analysis application using Flutter that evaluates daily lifestyle balance based on work/study time, sleep, screen usage, and social interaction. The application calculates a behavioral balance score and converts it into meaningful emotional insights and real-world suggestions, encouraging users to improve human connection and mental wellness. The system stores daily reflections locally, visualizes past patterns through a calm history dashboard, and operates entirely offline without user accounts, focusing on accessibility, privacy, and mindful technology usage.",
       longDescription: ".",
-      image: "https://i.pinimg.com/736x/93/65/cf/9365cf4e4b94af9fb86fb7f6540b4870.jpg",
+      image: "https://i.pinimg.com/736x/96/45/9b/96459b7663b92bc6704a79012b184955.jpg",
       tags: ["Flutter", "Dart"],
         links: { github: "https://github.com/tejasrmap/WorkLifeBalance-C4CHackathon", redirect: "#"}
     }
